e9e0a5aeb84a8a0f116fe1b35fb49ea5686e04e1
smarthand <none>
2019-01-26 01:20:14 +0000

    pyc -> py

--- lib/run_ur5_rtde.sh
+++ lib/run_ur5_rtde.sh
@@ -2,13 +2,13 @@
 
 function close_running(){
     echo "Cleaning up"
-    A=$(ps ax | grep -m1 'python2 ur5_rtde.pyc' | awk '{print $1}')
+    A=$(ps ax | grep -m1 'python2 ur5_rtde.py' | awk '{print $1}')
     kill $A
 }
 
 close_running
 
 cd ~/rmstudio/lib
-python2 ur5_rtde.pyc &
+python2 ur5_rtde.py &
 echo "Interfaces running"
 
